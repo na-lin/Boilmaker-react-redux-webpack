b583c99f015ff6acdb523efd04119f60f2bb9dee
Na Lin <lna483018@gmail.com>
2023-01-03 15:29:23 -0500

    connect redux to react app

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-
+import { Provider } from "react-redux";
+import store from "./store";
 const root = ReactDOM.createRoot(document.getElementById("app"));
 
-root.render(<h1>Hello world from react!</h1>);
+root.render(
+  <Provider store={store}>
+    <h1>Hello from react template!</h1>
+  </Provider>
+);
