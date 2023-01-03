76b40ed849e05418685f554494f2876f372bde66
Na Lin <lna483018@gmail.com>
2023-01-03 15:20:48 -0500

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  mode: "development",
+  entry: ["./index.js"],
+  output: {
+    path: __dirname,
+    filename: "./public/dist/bundle.js",
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+        },
+      },
+    ],
+  },
+};
