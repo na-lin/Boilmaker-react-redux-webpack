c8ef7eab695a520399b38072eac56b0fbc542ab7
Na Lin <lna483018@gmail.com>
2023-01-03 15:21:45 -0500

    add react dom script

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("app"));
+
+root.render(<h1>Hello world from react!</h1>);
